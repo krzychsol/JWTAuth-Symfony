e37d2a854dc983511e720c989608b3d496d3234a
Krzysztof Solecki <32575999+krzychsol@users.noreply.github.com>
2023-02-19 14:23:55 +0100

    Add files via upload

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $users = $doctrine->getrepository(User::class)->findAll();
+        return $this->json($users);
+    }
+}
